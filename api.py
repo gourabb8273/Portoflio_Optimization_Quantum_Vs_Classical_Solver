225ed61e5565febb2a1033a5a2927a07167e20f7
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2024-07-29 18:21:11 +0530

    adding style

--- api.py
+++ api.py
@@ -152,4 +152,4 @@ api.add_resource(QuantumOptimization, '/quantum')
 api.add_resource(Comparison, '/comparison')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
