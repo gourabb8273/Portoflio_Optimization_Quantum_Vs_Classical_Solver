9ec6913bcccd02c1ec36135e8cbc2c3b26b92a2b
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2024-07-29 20:53:51 +0530

    adding constraint

--- api.py
+++ api.py
@@ -164,4 +164,4 @@ api.add_resource(Comparison, '/comparison')
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable or default to 5000
     # app.run(port=port)
-    app.run(port=5000)
+    app.run(port=5005)
