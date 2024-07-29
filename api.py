eb9ea9cbf2280d4127022272a66b91ec5ffb01de
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2024-07-29 23:02:30 +0530

    adding risk

--- api.py
+++ api.py
@@ -167,4 +167,4 @@ api.add_resource(Comparison, '/comparison')
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable or default to 5000
     # app.run(port=port)
-    app.run(port=5000)
+    app.run(host="0.0.0.0",port=5000)
